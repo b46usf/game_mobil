ac48a32e885104e32d37ce9b2f6998ced65f7753
bagus farouk <b46usf@gmail.com>
2025-04-07 22:04:19 +0700

    commit

--- js/car.js
+++ js/car.js
@@ -1,9 +1,29 @@
 import { car, gameStarted } from './gameState.js';
-// import { GLTFLoader } from '../libs/GLTFLoader.js';
-// import { addCarHeadlights } from './lighting.js';
+import { GLTFLoader } from '../libs/GLTFLoader.js';
+import { addCarHeadlights } from './lighting.js';
 // import { animate } from './gameLoop.js';
 
 export function loadCar(scene, mode) {
-  console.log('Loading car...');
-  console.log(car, gameStarted, scene, mode);
+  const loader = new GLTFLoader();
+  loader.load('model/Turbo_Star_Car.glb', gltf => {
+    car = gltf.scene;
+    car.scale.set(1.5, 1.5, 1.5);
+    car.rotation.y = Math.PI;
+    car.position.set(0, 1, 0);
+    scene.add(car);
+
+    // Tambahkan lampu mobil jika malam
+    if (mode === 'malam') {
+      addCarHeadlights(car, scene);
+    }
+
+    if (!gameStarted) {
+        gameStarted = true;
+        // animate();
+    }
+
+    console.log('Loading car...');
+    console.log(car, gameStarted, scene, mode);
+    // Jalankan callback jika sudah dimuat (misalnya mulai animasi)
+  });
 }
