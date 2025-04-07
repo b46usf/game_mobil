f9c5874234e8d4003d0c6599a886d55adc751a6e
bagus farouk <b46usf@gmail.com>
2025-04-07 13:17:34 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import { initScene } from './init.js';
-import { animate } from './gameLoop.js';
-import { setupUI } from './ui.js';
-import { setupInput } from './input.js';
-import { playBackgroundMusic } from './sound.js';
+import { initScene } from 'init.js';
+import { animate } from 'gameLoop.js';
+import { setupUI } from 'ui.js';
+import { setupInput } from 'input.js';
+import { playBackgroundMusic } from 'sound.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initScene();
